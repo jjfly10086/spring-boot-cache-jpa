fa047caa9038810da9a3448e6be66968fa070ac7
jiangwenhua <jiangwenhua@lechebang.com>
2018-01-05 10:56:49 +0800

    update

--- src/main/java/com/jwh/demo/StartApplication.java
+++ src/main/java/com/jwh/demo/StartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableCaching
-@ImportResource(locations = "classpath:*applicationContext.xml")
+@ImportResource(locations = "classpath:spring/applicationContext.xml")
 public class StartApplication {
 
     public static void main(String[] args){
