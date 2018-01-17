eb5263a604d18fcde78ae8acd994a5cdc0734a1f
jiangwenhua <jiangwenhua@lechebang.com>
2018-01-17 17:21:35 +0800

    update

--- src/main/java/com/jwh/demo/repository/UserRepository.java
+++ src/main/java/com/jwh/demo/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.jwh.demo.repository;
 
 import com.jwh.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,5 +15,10 @@ public interface UserRepository extends JpaRepository<User,Long> {
     //自定义接口命名
     @Query("select id,userName,userPass from User where user_name =:name")//JPQL语法
     List<User> findUserList(@Param("name") String userName);
+
+    //nativeQuery=true原生sql
+    @Modifying
+    @Query(value = "INSERT INTO `user` (`id`,`user_name`,`user_pass`) VALUES (?1,?2,?3)",nativeQuery = true)
+    int insert(Long id,String userName,String userPass);
 
 }
